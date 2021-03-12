03a2195883516c4e03eca0afd7260828408b82c9
atul-sachan <34751474+atul-sachan@users.noreply.github.com>
2021-03-12 22:20:50 +0530

    Initial commit

---
+++ WebApiDemo/WebApiDemo/Controllers/AuthorsController.cs
@@ -0,0 +1,45 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApiDemo.Helpers;
+using WebApiDemo.Models;
+using WebApiDemo.Services;
+
+namespace WebApiDemo.Controllers
+{
+    [Route("api/authors")]
+    [ApiController]
+    public class AuthorsController : ControllerBase
+    {
+        private readonly ICourseLibraryRepository courseLibraryRepository;
+        private readonly IMapper mapper;
+
+        public AuthorsController(ICourseLibraryRepository courseLibraryRepository, IMapper mapper)
+        {
+            this.courseLibraryRepository = courseLibraryRepository ?? throw new ArgumentNullException(nameof(courseLibraryRepository));
+            this.mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<AuthorDto>> GetAuthors()
+        {
+            var authorsFromRepo = this.courseLibraryRepository.GetAuthors();
+            return Ok(this.mapper.Map<IEnumerable<AuthorDto>>(authorsFromRepo));
+        }
+
+        [HttpGet("{authorId:guid}")]
+        public IActionResult GetAuthor(Guid authorId)
+        {
+            var authorFromRepo = this.courseLibraryRepository.GetAuthor(authorId);
+            if (authorFromRepo == null)
+            {
+                return NotFound();
+            }
+            return Ok(authorFromRepo);
+        }
+    }
+}
---
+++ WebApiDemo/WebApiDemo/Controllers/CoursesController.cs
@@ -0,0 +1,44 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApiDemo.Models;
+using WebApiDemo.Services;
+
+namespace WebApiDemo.Controllers
+{
+    [Route("api/authors/{authorId}/courses")]
+    [ApiController]
+    public class CoursesController : ControllerBase
+    {
+        private readonly ICourseLibraryRepository courseLibraryRepository;
+        private readonly IMapper mapper;
+
+        public CoursesController(ICourseLibraryRepository courseLibraryRepository, IMapper mapper)
+        {
+            this.courseLibraryRepository = courseLibraryRepository ?? throw new ArgumentNullException(nameof(courseLibraryRepository));
+            this.mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<CourseDto>> GetCoursesFromAuthor(Guid authorId)
+        {
+            var courses = this.courseLibraryRepository.GetCourses(authorId);
+            return Ok(this.mapper.Map<IEnumerable<CourseDto>>(courses));
+        }
+
+        [HttpGet("{courseId:guid}")]
+        public IActionResult GetCourseFromAuthor(Guid authorId, Guid courseId)
+        {
+            var course = this.courseLibraryRepository.GetCourse(authorId, courseId);
+            if (course == null)
+            {
+                return NotFound();
+            }
+            return Ok(course);
+        }
+    }
+}
